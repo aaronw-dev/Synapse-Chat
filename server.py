ba60360a6a4665e85e9635befd436a885b694c18
AW_Dev <riptidegamesceo@gmail.com>
2023-10-24 07:54:06 -0700

    cookie functions and avatar generation

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import json
+import threading
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit
 from colors import *
@@ -12,6 +13,9 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import random
 import string
+import hashlib
+from generate_avatar import generateavatar
+import time
 
 cred = credentials.Certificate('serverside/creds.json')
 app = firebase_admin.initialize_app(cred)
@@ -21,8 +25,11 @@ os.system("")
 app = Flask(__name__)
 socketio = SocketIO(app, ping_timeout=5000, ping_interval=10000)
 
-connectedusers = {}
+with open("serverside/avatarapi.key", "r") as file:
+    avatarapikey = file.read()
 
+connectedusers = {}
+activecookies = []
 
 '''sendEmail(
     "Johnny Bellwood",
@@ -44,6 +51,25 @@ def generate_id(length):
     characters = string.ascii_letters + string.digits
     result = ''.join(random.choice(characters) for _ in range(length))
     return result
+
+
+def expire_cookie(expiration_time, cookie):
+    global activecookies
+    start_time = time.time()
+
+    variable_expired = False
+    while not variable_expired:
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= expiration_time:
+            variable_expired = True
+            activecookies.remove(cookie)
+        time.sleep(1)
+
+
+def generate_cookie_string(length):
+    characters = string.ascii_letters + string.digits
+    cookie = ''.join(random.choice(characters) for _ in range(length))
+    return cookie
 
 
 @app.route('/')
@@ -71,11 +97,14 @@ def login():
         if (email == userinfo["email"] and password == userinfo["password"]):
             userauthenticated = True
     if (userauthenticated):
-        print("Authentication success.")
-        return json.dumps({"auth_result": "success"})
+        cookie = generate_cookie_string(69)
+        activecookies.append(cookie)
+        expiration_thread = threading.Thread(
+            target=lambda: expire_cookie(expiration_time=3600, cookie=cookie))
+        expiration_thread.start()
+        return json.dumps({"auth_result": "success", "cookie": cookie})
     else:
-        print("Authentication failed.")
-        return json.dumps({"auth_result": "failed"})
+        return json.dumps({"auth_result": "failed", "cookie": ""})
 
 
 @app.route('/signup', methods=["POST"])
@@ -83,13 +112,12 @@ def signup():
     username = request.form["username"]
     email = request.form["email"]
     password = request.form["password"]
+    password = hashlib.sha256(bytes(password, "ascii")).hexdigest()
     generatedid = generate_id(20)
     # Save profile image full resolution
     newpath = f'static/imagestore/users/{generatedid}'
     if not os.path.exists(newpath):
         os.makedirs(newpath)
-    profileimage = request.files['profilepic']
-    profileimage.save(f"static/imagestore/users/{generatedid}/fullres.png")
 
     # Add user to FireStore
     userdata = {
@@ -100,18 +128,23 @@ def signup():
     db.collection("accounts").document(generatedid).set(userdata)
 
     # Profile picture creation
-    image = Image.open(f'static/imagestore/users/{generatedid}/fullres.png')
+    image = generateavatar(apikey=avatarapikey, name=username)
     if (image.width > image.height):
         squareimage = crop_center(image, image.height, image.height)
     else:
         squareimage = crop_center(image, image.width, image.width)
-    squareimage.save(f'static/imagestore/users/{generatedid}/square.png')
+    squareimage.save(f'static/imagestore/users/{generatedid}/512x512.png')
     fifty = squareimage.resize((50, 50))
     fifty.save(f'static/imagestore/users/{generatedid}/50x50.png')
     hundo = squareimage.resize((100, 100))
     hundo.save(f'static/imagestore/users/{generatedid}/100x100.png')
 
     return "Successfully signed up."
+
+
+@socketio.on('message')
+def landing_page(payload):
+    print(payload["message"])
 
 
 @socketio.on('ping')
